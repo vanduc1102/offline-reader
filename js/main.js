d488f8f78482f125cab1332bd812b2fda6e02704
DUC NGUYEN <vanducld@gmail.com>
2015-06-01 00:16:46 +0700

    #3 read rss news, of vnepxress and tinhte.vn

--- js/main.js
+++ js/main.js
@@ -2,40 +2,53 @@ $(function(){
 	$('input').on('click',function(){
 		$('body').append('<h1>Hello </h1>');
 	});
-	$.ajax({
-		url: "http://giaitri.vnexpress.net/tin-tuc/gioi-sao/quoc-te/chang-duong-6-nam-yeu-nhau-cua-hieu-minh-angelababy-3225188.html"
-		,context: document.body,
-		dataType:'html'
-	}).done(function(data) {
-		var webPage = $(data);
-		var contentPart = webPage.find("div.main_content_detail");
-		var titleNews = contentPart.find("div.title_news").find("h1").text();
-		$("#content").append("<h1>"+titleNews+"</h1>");
-		var shortIntro = contentPart.find("div.short_intro");
-		$("#content").append(shortIntro);
-		var mainContent = contentPart.find("div.fck_detail.width_common");
-	    mainContent.find("img").each(function(idx){
-	    	$(this).addClass("img-responsive");
-	    });
-		$("#content").append(mainContent);
-	});
+	rssUrl = 'http://www.tinhte.vn/rss';
+    $.get(rssUrl, function(data) {
+    	var $XML = $(data);
+	    $XML.find("item").each(function() {
+	        var $this = $(this),
+	            item = {
+	                title:       $this.find("title").text(),
+	                link:        $this.find("link").text(),
+	                description: $this.find("description").text(),
+	                pubDate:     $this.find("pubDate").text(),
+	                author:      $this.find("author").text()
+	            };
+	            console.log(item);
+				$.ajax({
+					url: item.link,
+					context: document.body,
+					dataType:'html'
+				}).then(function (response){
+					// var data = response;
+					// var webPage = $(data);
+					// var contentPart = webPage.find("div.main_content_detail");
+					// var titleNews = contentPart.find("div.title_news").find("h1").text();
+					// $("#content").append("<h1>"+titleNews+"</h1>");
+					// var shortIntro = contentPart.find("div.short_intro");
+					// $("#content").append(shortIntro);
+					// var mainContent = contentPart.find("div.fck_detail.width_common");
+				 //    mainContent.find("img").each(function(idx){
+				 //    	$(this).addClass("img-responsive");
+				 //    });
+				//$("#content").append(mainContent);
 
-	$.ajax({
-    	url: "http://www.bloomberg.com/news/articles/2015-05-27/the-wrath-of-hank-ex-aig-ceo-greenberg-won-t-give-up-at-age-90"
-    	,context: document.body,
-    	dataType:'html'
-  	}).done(function(data) {
-    	var webPage = $(data);
-	    var contentPart = webPage.find("main#content");
-	    var titleNews = contentPart.find("span.lede-headline__highlighted").text();
-	    $("#content").append("<h1>"+titleNews+"</h1>");
-	    //var shortIntro = contentPart.find("div.short_intro");
-	    //$("#content").append(shortIntro);
-	    var mainContent = contentPart.find("section.article-body");
-	    contentPart.find("img").each(function(idx){
-	    	$(this).addClass("img-responsive");
+
+					var data = response;
+					var webPage = $(data);
+					var contentPart = webPage.find("div#content");
+
+					$("#content").append("<h1>"+item.title+"</h1>");
+
+					var mainContent = contentPart.find("div.messageInfo.primaryContent:first");
+				    mainContent.find("img").each(function(idx){
+				    	$(this).addClass("img-responsive");
+				    });
+
+					$("#content").append(mainContent);
+				}, function(reason){
+
+				});
 	    });
-	    $("#content").append(mainContent);
-  });
-	
+	});	
 });
---
+++ js/rss-links.js
@@ -0,0 +1,14 @@
+var PAGE_LINKS={
+	"vnexpress":{
+		"rss":[
+			"http://vnexpress.net/rss/tin-moi-nhat.rss"
+		],
+		"contentTag":"div.main_content_detail > div.fck_detail.width_common"
+	},
+	"tinhte":{
+		"rss":[
+			"http://www.tinhte.vn/rss"
+		],
+		"contentTag":"div#content > div.messageInfo.primaryContent:first"
+	}
+};
