04f725b99e1b2ab5b21997f7c1b62c1b0a5ae824
DUC NGUYEN <vanducld@gmail.com>
2015-06-01 23:44:54 +0700

    #3 - read all posts of pages

--- js/main.js
+++ js/main.js
@@ -1,54 +1,40 @@
 $(function(){
-	$('input').on('click',function(){
-		$('body').append('<h1>Hello </h1>');
-	});
-	rssUrl = 'http://www.tinhte.vn/rss';
-    $.get(rssUrl, function(data) {
-    	var $XML = $(data);
-	    $XML.find("item").each(function() {
-	        var $this = $(this),
-	            item = {
-	                title:       $this.find("title").text(),
-	                link:        $this.find("link").text(),
-	                description: $this.find("description").text(),
-	                pubDate:     $this.find("pubDate").text(),
-	                author:      $this.find("author").text()
-	            };
-	            console.log(item);
-				$.ajax({
-					url: item.link,
-					context: document.body,
-					dataType:'html'
-				}).then(function (response){
-					// var data = response;
-					// var webPage = $(data);
-					// var contentPart = webPage.find("div.main_content_detail");
-					// var titleNews = contentPart.find("div.title_news").find("h1").text();
-					// $("#content").append("<h1>"+titleNews+"</h1>");
-					// var shortIntro = contentPart.find("div.short_intro");
-					// $("#content").append(shortIntro);
-					// var mainContent = contentPart.find("div.fck_detail.width_common");
-				 //    mainContent.find("img").each(function(idx){
-				 //    	$(this).addClass("img-responsive");
-				 //    });
-				//$("#content").append(mainContent);
+	for(var i = 0 ; i < PAGE_LINKS.length ; i++){
+		var languagePage = PAGE_LINKS[i];
+		for(var j = 0 ; j < languagePage.newsPages.length ; j++){
+			var newsPage = languagePage.newsPages[j];
+			feedNewsByRSS(newsPage.rss,newsPage.contentTag);
+		}
+	}
+	function feedNewsByRSS(rssURL, contentTag){
+	    $.get(rssURL, function(data) {
+	    	var $XML = $(data);
+		    $XML.find("item").each(function() {
+		        var $this = $(this),
+		            item = {
+		                title:       $this.find("title").text(),
+		                link:        $this.find("link").text(),
+		                description: $this.find("description").text(),
+		                pubDate:     $this.find("pubDate").text(),
+		                author:      $this.find("author").text()
+		            };
+					$.ajax({
+						url: item.link,
+						context: document.body,
+						dataType:'html'
+					}).then(function (data){
+						var webPage = $(data);
+						$("#content").append("<h1>"+item.title+"</h1>");
+						var contentElement  = webPage.find(contentTag);				
+						var mainContent = contentElement.length > 1 ? $(contentElement.get(0)) : contentElement ;
+					    mainContent.find("img").each(function(idx){
+					    	$(this).addClass("img-responsive");
+					    });
+						$("#content").append(mainContent);
+					}, function(reason){
 
-
-					var data = response;
-					var webPage = $(data);
-					var contentPart = webPage.find("div#content");
-
-					$("#content").append("<h1>"+item.title+"</h1>");
-
-					var mainContent = contentPart.find("div.messageInfo.primaryContent:first");
-				    mainContent.find("img").each(function(idx){
-				    	$(this).addClass("img-responsive");
-				    });
-
-					$("#content").append(mainContent);
-				}, function(reason){
-
-				});
-	    });
-	});	
+					});
+		    });
+		});	
+	};
 });
--- js/rss-links.js
+++ js/rss-links.js
@@ -1,14 +1,21 @@
-var PAGE_LINKS={
-	"vnexpress":{
-		"rss":[
-			"http://vnexpress.net/rss/tin-moi-nhat.rss"
-		],
-		"contentTag":"div.main_content_detail > div.fck_detail.width_common"
+var PAGE_LINKS=[
+	{	
+		"language":"vietnamese",
+		"newsPages":[
+			{
+				"pageTitle":"",
+				"rss":"http://vnexpress.net/rss/tin-moi-nhat.rss",
+				"contentTag":"div.main_content_detail div.fck_detail.width_common"
+			},
+			{
+				"pageTitle":"",
+				"rss":"http://www.tinhte.vn/rss",
+				"contentTag":"div.mainContent div.messageContent article"
+			}
+		]
 	},
-	"tinhte":{
-		"rss":[
-			"http://www.tinhte.vn/rss"
-		],
-		"contentTag":"div#content > div.messageInfo.primaryContent:first"
-	}
-};
+	{
+		"language":"english",
+		"newsPages":[]
+	}	
+];
